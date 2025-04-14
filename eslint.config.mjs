e3a8bbbcea2d2455bdf6234b91a74317673acc21
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2025-04-14 13:09:18 +0200

    chore: setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,21 +10,21 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
   {
-    files: ["**/*.ts", "**/*.tsx"],
+    files: ['**/*.ts', '**/*.tsx'],
     rules: {
-      "import/order": [
-        "error",
+      'import/order': [
+        'error',
         {
-          groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object"],
-          "alphabetize": { order: "asc", caseInsensitive: true },
-          "newlines-between": "always"
-        }
-      ]
-    }
-  }
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
